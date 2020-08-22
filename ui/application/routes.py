94fce4d7d30d993007395658dc6f8679d407ffef
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-22 16:38:09 +0100

    Attempting to resolve JSON error.

--- ui/application/routes.py
+++ ui/application/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, jsonify
 from application import app
 import requests
+import json
 
 @app.route('/', methods=['GET'])
 def home():
@@ -11,7 +12,8 @@ def prize():
 	letter = requests.get('http://lgen:5001/prize/lgen')
 	number = requests.get('http://ngen:5002/prize/ngen')
 	account = {"Letter Value":letter, "Number Value":number}
-	prize = requests.post('http://pgen:5003/prize/pgen', json=account)
+	jsonData = json.dumps(account)
+	prize = requests.post('http://pgen:5003/prize/pgen', json=jsonData)
 	print(prize.status_code)
 	#prize = requests.get('http://pgen:5003/prize/pgen')
 	return render_template('prize.html', title='Prize', prize=prize.text)
