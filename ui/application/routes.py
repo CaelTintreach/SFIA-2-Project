98d82d0b0c76cda6c921b409b3465cf4fc9b3a4a
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-22 16:51:15 +0100

    Trying to pinpoint error source.

--- ui/application/routes.py
+++ ui/application/routes.py
@@ -9,11 +9,14 @@ def home():
 
 @app.route('/get/prize', methods=['GET','POST'])
 def prize():
-	letter = requests.get('http://lgen:5001/prize/lgen')
-	number = requests.get('http://ngen:5002/prize/ngen')
-	account = {"Letter Value":letter, "Number Value":number}
-	jsonData = json.dumps(account)
-	prize = requests.post('http://pgen:5003/prize/pgen', json=jsonData)
-	print(prize.status_code)
+	try:
+		letter = requests.get('http://lgen:5001/prize/lgen')
+		number = requests.get('http://ngen:5002/prize/ngen')
+		account = {"Letter Value":letter, "Number Value":number}
+		jsonData = json.dumps(account)
+		prize = requests.post('http://pgen:5003/prize/pgen', json=jsonData)
+		print(prize.status_code)
 	#prize = requests.get('http://pgen:5003/prize/pgen')
-	return render_template('prize.html', title='Prize', prize=prize.text)
+		return render_template('prize.html', title='Prize', prize=prize.text)
+	except Exception as e:
+		print(e)  
