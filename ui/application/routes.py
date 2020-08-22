fb62b186386a353c47578e117351ea675254ee6d
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-22 17:23:43 +0100

    Still resolving JSON error.

--- ui/application/routes.py
+++ ui/application/routes.py
@@ -9,12 +9,8 @@ def home():
 
 @app.route('/get/prize', methods=['GET','POST'])
 def prize():
-	try:
-		letter = requests.get('http://lgen:5001/prize/lgen')
-		number = requests.get('http://ngen:5002/prize/ngen')
-		prize = requests.post('http://pgen:5003/prize/pgen', json={"Letter":letter.text, "Number":number.text})
-		print(prize.status_code)
-		#prize = requests.get('http://pgen:5003/prize/pgen')
-		return render_template('prize.html', title='Prize', prize=prize.text)
-	except Exception as e:
-		print(e)  
+	letter = requests.get('http://lgen:5001/prize/lgen')
+	number = requests.get('http://ngen:5002/prize/ngen')
+	prize = requests.post('http://pgen:5003/prize/pgen', json={"Letter":letter.text, "Number":number.text})
+	print(prize.status_code)
+	return render_template('prize.html', title='Prize', prize=prize.text)
