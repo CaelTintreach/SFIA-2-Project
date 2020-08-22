342c2c3734a54067d54aea1e1b8f53fc27b99457
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-22 16:20:56 +0100

    Attempting to fix error within nGen.

--- ngen/application/routes.py
+++ ngen/application/routes.py
@@ -2,6 +2,7 @@ from application import app
 from flask import request, Response 
 import random
 
+'''
 @app.route('/prize/ngen', methods = ['GET'])
 def nGen():
 	number = random.randint(1,10)
@@ -9,3 +10,9 @@ def nGen():
 	return Response(number, mimetype='text/plain')
 
 print(nGen())
+'''
+
+@app.route('/prize/lgen', methods = ['GET'])
+def lGen():
+	prizePool = ["A", "B"]
+	return Response(random.choice(prizePool), mimetype='text/plain')
--- pgen/application/routes.py
+++ pgen/application/routes.py
@@ -7,16 +7,16 @@ def prize():
 	data = request.get_json()
 	prizeVal = ""
 	if data["Letter Value"] == "A":
-		if data["Number Value"] <= 5:
+		if data["Number Value"] == "A":
 			prizeVal = "no prize!"
 			return Response(prizeVal, mimetype='text/plain')
-		if data["Number Value"]  > 5:
+		if data["Number Value"]  == "B":
 			prizeVal = "a small prize!"
 			return Response(prizeVal, mimetype='text/plain')
 	if data["Letter Value"] == "B":
-		if data["Number Value"]  <= 5:
+		if data["Number Value"]  == "A":
 			prizeVal = "a small prize!"
 			return Response(prizeVal, mimetype='text/plain')
-		if data["Number Value"]  > 5:
+		if data["Number Value"]  == "B":
 			prizeVal = "a big prize!"
 			return Response("You won" + prizeVal, mimetype='text/plain')
