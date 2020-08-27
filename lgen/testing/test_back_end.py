f1db8b837f4df86029a3e7f233310cf83a07001d
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-27 09:35:49 +0100

    Adding testing base.

--- lgen/testing/test_back_end.py
+++ lgen/testing/test_back_end.py
@@ -1,31 +1,18 @@
-import pytest, unittest
 from unittest.mock import patch
-from application import app
-from flask import abort, url_for
+from flask import url_for
 from flask_testing import TestCase
-from os import getenv
+from application import app
 
 class TestBase(TestCase):
+	def create_app(self):
+		config_name = 'testing'
+		return app
 
-    def create_app(self):
-        config_name = 'test'
-        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('TEST_DB_URI'),
-            SECRET_KEY=getenv('TEST_SECRET_KEY'),
-            WTF_CSRF_ENABLED=False,
-            DEBUG=True
-            )
-        return app
-
-    def setUp(self):
-        db.session.commit()
-        db.drop_all()
-        db.create_all()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-class TestViews(TestBase):    
-    def test_lgen_view(self):
-        response = self.client.get(url_for('/prize/lgen'))
-        self.assertEqual(response.status_code, 200)
+class TestCountryName(TestBase):
+	def test_countrynames(self):
+		response = self.client.get(url_for('/prize/lgen'))
+		check = False
+		for item in ['A','B']:
+			if bytes.decode(response.data) == item:
+				check = True
+		self.assertTrue(check)
